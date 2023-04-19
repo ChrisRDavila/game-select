bb181e7608c1036bc7d075b752b58427ebba7ee1
ChrisRDavila <crossdavila@gmail.com>
2023-04-19 10:35:26 -0700

    rebase repo

--- src/game-select.js
+++ src/game-select.js
@@ -5,7 +5,7 @@ export default class GameSelect {
       const url = `https://www.freetogame.com/api/games?category`;
       request.addEventListener("load", function() {
         if (this.status === 200) {
-          const categories = JSON.parse(this.responseText).game_categories;
+          const categories = JSON.parse(request.responseText);
           resolve(categories);
         } else {
           reject(Error(this.statusText));
